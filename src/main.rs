0c0fd557d21b6c181734707d67167c123ea699dd
Kirill Borisov <lensvol@gmail.com>
2016-03-19 17:59:16 +0300

    More robust handling of errors and missing consumer_token.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate toml;
 extern crate rand;
 
 use pocket::Pocket;
-use rustc_serialize::{Decodable, Encodable};
+use rustc_serialize::Encodable;
 use rand::distributions::Range;
 use rand::distributions::IndependentSample;
 
@@ -15,15 +15,25 @@ use std::fs::{File, OpenOptions};
 use std::str::from_utf8;
 use toml::{decode_str, encode_str};
 
+
+fn read_line(prompt: &str) -> String {
+    print!("{}", prompt);
+
+    let mut line_input: String = "".to_owned();
+    match io::stdin().read_line(&mut line_input) {
+        Ok(_) => line_input,
+        Err(_) => "".to_owned(),
+    }
+}
+
 fn authorize(consumer_key: &str) -> Pocket {
     let mut pocket: Pocket = Pocket::new(consumer_key, None);
     let url = pocket.get_auth_url().unwrap();
 
     println!("Follow this link to authorize the app: {}", url);
 
-    let mut line_input: String = "".to_owned();
-    io::stdin().read_line(&mut line_input).ok();
-    
+    read_line("");
+
     pocket.authorize().expect("Authorization failed!");
     pocket
 }
@@ -44,50 +54,77 @@ pub struct Settings {
 fn load_cfg(filename: &str) -> Option<Settings> {
     let mut f = File::open(filename).unwrap();
     let mut content: Vec<u8> = vec![];
-    f.read_to_end(&mut content);
+    match f.read_to_end(&mut content) {
+        Ok(_) => {
+            let settings = decode_str(from_utf8(&content).unwrap()).unwrap();
+            Some(settings)
+        }
+        Err(_) => None,
+    }
 
-    let settings = decode_str(from_utf8(&content).unwrap()).unwrap();
-    Some(settings)
 }
 
-fn save_cfg(filename: &str, cfg: &Settings) {
+fn save_cfg(filename: &str, cfg: &Settings) -> Result<usize, io::Error> {
     let content: String = encode_str(&cfg);
 
     let mut file = OpenOptions::new()
-        .write(true)
-        .create(true)
-        .open(filename)
-        .unwrap();
+                       .write(true)
+                       .create(true)
+                       .open(filename)
+                       .unwrap();
 
-    file.write_all(content.as_bytes());
+    try!(file.write_all(content.as_bytes()));
+
+    Ok(content.len())
 }
 
 fn acquire_pocket_instance(cfg: &Settings) -> Pocket {
     let consumer_key = &cfg.credentials.consumer_key;
 
     if let Some(ref token) = cfg.credentials.access_token {
-        Pocket::new(
-            &consumer_key,
-            Some(&token),
-        )
+        Pocket::new(&consumer_key, Some(&token))
     } else {
         authorize(&consumer_key)
     }
 }
 
 fn main() {
-    let mut cfg = load_cfg("archeopocket.toml").unwrap();
+    let mut need_save_settings = false;
+
+    let mut cfg = match load_cfg("archeopocket.toml") {
+        Some(cfg) => cfg,
+        None => {
+            let consumer_key = read_line("Enter Pocket consumer key: ");
+            need_save_settings = true;
+            Settings {
+                credentials: Credentials {
+                    access_token: None,
+                    consumer_key: consumer_key,
+                },
+                max_count: None,
+                last_items: None,
+            }
+        }
+    };
 
     let mut pocket = acquire_pocket_instance(&cfg);
+
     if cfg.credentials.access_token.is_none() {
         let token = pocket.access_token().unwrap().to_owned();
         cfg.credentials.access_token = Some(token);
-        save_cfg("archeopocket.toml", &cfg);
+        need_save_settings = true;
+    }
+
+    if need_save_settings {
+        match save_cfg("archeopocket.toml", &cfg) {
+            Ok(count) => println!("Wrote {} bytes to configuration file.", count),
+            Err(e) => println!("Failed to save configuration file: {}", e),
+        }
     }
 
     let num_last_items = cfg.last_items.unwrap_or(250);
     let max_count = cfg.max_count.unwrap_or(5);
-    
+
     let items = {
         let mut f = pocket.filter();
         f.simple();
@@ -100,11 +137,13 @@ fn main() {
 
     let between = Range::new(0, num_last_items - 1);
     let mut rng = rand::thread_rng();
-    
+
     for _ in 0..max_count {
         let index = between.ind_sample(&mut rng);
         let item = &result[index];
-        println!("Title: {}\nURL: {}\n", item.resolved_title, item.resolved_url);
+        println!("Title: {}\nURL: {}\n",
+                 item.resolved_title,
+                 item.resolved_url);
     }
 
 }
